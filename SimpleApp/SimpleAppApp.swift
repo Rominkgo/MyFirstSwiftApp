c669c54b3f355e9a0f82102d91a0d3bd0f218b9c
rominkgo <rominkgo@gmail.com>
2022-11-15 14:02:16 -0500

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Default on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
